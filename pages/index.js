ff2ff268677b4601424bdcc4c58d322604e711c6
Pranudech <pranudech@gmail.com>
2023-06-29 12:07:45 +0700

    fixbug

--- pages/index.js
+++ pages/index.js
@@ -29,11 +29,11 @@ export default function Home({ posts }) {
         width="100%"
         height='980px'
         styles={{ left: 0 }}
-      /> */}
+      /> */} 
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="hidden sm:mb-8 sm:flex sm:justify-center">
           <div className="relative rounded-full px-3 py-1 text-xl leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-            WELCOME TO THE
+            WELCOME TO THE .
           </div>
         </div>
         <div className="text-center">
